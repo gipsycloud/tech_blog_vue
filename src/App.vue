4e36559c94925fb71809a1dcc0bf2fb58c0bf882
aungwin <aungthiwin@sys-city.com>
2019-04-26 17:24:00 +0630

    view output by api(json format)

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,17 @@
   </div> -->
   <div id="app">
     <Header />
-    <QuestionBox />
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col sm="6" offset="3">
+          <QuestionBox
+            v-if="questions.length"
+            :currentQuestion="questions[index]" 
+            :next="next"
+            />
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
@@ -19,6 +29,28 @@ export default {
   components: {
     Header,
     QuestionBox
+  },
+  data() {
+    return {
+      questions: [],
+      index: 0
+    }
+  },
+  methods: {
+    next() {
+      this.index++
+    }
+  },
+  mounted: function() {
+    fetch('https://opentdb.com/api.php?amount=10&category=21&difficulty=medium&type=multiple', {
+      method: 'get'
+    })
+    .then((response) => {
+      return response.json()
+    })
+    .then((jsonData) => {
+      this.questions = jsonData.results
+    })
   }
 }
 </script>
