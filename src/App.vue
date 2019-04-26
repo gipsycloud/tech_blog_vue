34433881153407d2b913801601f5c8d9494039a1
aungwin <aungthiwin@sys-city.com>
2019-04-26 15:25:29 +0630

    adding bootstrap with npm

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
-  <div id="app">
+  <!-- <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div> -->
+  <div id="app">
+    <Header />
+    <QuestionBox />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import QuestionBox from './components/QuestionBox.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header,
+    QuestionBox
   }
 }
 </script>
